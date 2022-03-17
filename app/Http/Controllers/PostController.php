7f28b5db883b6a4cca5327c5d23cfaeeca944782
kashif.hussain <kashiemails@gmail.com>
2022-03-17 13:46:32 +0500

    mass assignmnet

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\support\Facades\DB;
+use App\Models\Post;
+
+class postController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id)
+    {
+        //
+        return "this is controller ".$id;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return " this is the id".$id;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function contact(){
+        
+        $people=['kashif','Wasib','M Faraz'];
+        return view('contact', compact('people'));
+    }
+
+    public function insert($title, $name){
+        
+        DB::insert('insert into posts(title, content) values(?,?)',[$title,$name]);
+    }
+
+    public function read(){
+        
+        $results = DB::select('select * from posts where id = ?',[1]);
+
+    // $reults = DB::table('posts')->select('*')->get();
+        foreach ( $results as $post){
+            return $post->title;
+        }
+    }
+
+    public function updatee($id, $title){
+        
+        $update = DB::update("update posts set title = '$title' where id = ?",[$id]);
+        return $update;
+
+    
+    }
+
+    public function deletee($title){
+        
+        $delete = DB::delete("delete from posts where title = ?",[$title]);
+        return $delete;
+
+    
+    }
+
+    public function find(){
+        
+        
+       // $posts = Post::where('id',3)->orderBy('id','desc')->take(1)->get();
+        $posts =Post::all();
+
+
+       // foreach($posts as $post){
+            //$post->title,$post->content;
+           // $postss=[$post->title,$post->content]
+           //$k= [$post->title,$post->content];
+           
+            //dd('hello');
+            //return $post->title;
+            return view('find', compact('posts'));
+        
+        
+
+    
+    }
+
+    public function binsert (){
+        
+       
+         $post = new Post;
+         $post->title='PHP framework?';
+         $post->content='Laravel ins the framewokr of PHP';
+         $post->Save();
+
+ 
+ 
+     }
+    
+
+     public function binsert2 ($id){
+        
+       
+        $post = Post::find($id);
+        $post->title='PHP framework?';
+        $post->content='Laravel ins the framewokr of PHP';
+        $post->Save();
+
+
+
+    }
+
+
+    public function createM(){
+        
+       
+        Post::create(['title'=>'create','content'=>'Create and config mass asssigment']);
+        
+
+
+
+    }
+   
+
+    public function show_post($id,$name){
+        //return view('posts')->with('id',$id);
+        return view('posts',compact('id','name'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::resource('posts','\App\Http\Controllers\PostController');
+
+Route::get('contact','\App\Http\Controllers\PostController@contact');
+
+Route::get('/insert/{title}/{name}','\App\Http\Controllers\PostController@insert');
+
+Route::get('/read','\App\Http\Controllers\PostController@read');
+
+Route::get('/update/{id}/{title}','\App\Http\Controllers\PostController@updatee');
+
+Route::get('/delete/{title}','\App\Http\Controllers\PostController@deletee');
+
+Route::get('/find','\App\Http\Controllers\PostController@find');
+
+Route::get('/binsert2/{id}','\App\Http\Controllers\PostController@binsert2');
+Route::get('/createM','\App\Http\Controllers\PostController@createM');
+
+
+Route::get('posts/{id}/{name}','\App\Http\Controllers\PostController@show_post');
+//Route::get('/posts/{id}', '\App\Http\Controllers\PostController');
+
+
+// Route::get('/about', function () {
+//     return 'about';
+// });
+
+// Route::get('/contact', function () {
+//     return 'welcome';
+// });
+
+// Route::get('/posts/{id}/{name}', function ($id,$name) {
+//     return 'Post number '. $id. " ". $name;
+// });
